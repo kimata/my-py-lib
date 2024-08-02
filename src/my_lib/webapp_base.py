4cd479cc55ce61e0b8f7f3447b5ae0ca7c422782
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-08-02 18:33:51 +0900

    fix

--- src/my_lib/webapp_base.py
+++ src/my_lib/webapp_base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
+import my_lib.flask_util
 from flask import Blueprint, redirect, send_from_directory
-from flask_util import gzipped
 from webapp_config import APP_URL_PREFIX, STATIC_FILE_PATH
 
 blueprint = Blueprint("webapp-base", __name__, url_prefix=APP_URL_PREFIX)
@@ -8,7 +8,7 @@ blueprint = Blueprint("webapp-base", __name__, url_prefix=APP_URL_PREFIX)
 
 @blueprint.route("/", defaults={"filename": "index.html"})
 @blueprint.route("/<path:filename>")
-@gzipped
+@my_lib.flask_util.gzipped
 def webapp(filename):
     return send_from_directory(STATIC_FILE_PATH, filename)
 
@@ -17,6 +17,6 @@ blueprint_default = Blueprint("webapp-default", __name__)
 
 
 @blueprint_default.route("/")
-@gzipped
+@my_lib.flask_util.gzipped
 def root():
     return redirect(f"{APP_URL_PREFIX}/")
