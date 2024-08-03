5ff50d4ad19b3b1410effa66ac2eb39ddd546a48
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-08-03 12:31:46 +0900

    fix

--- src/my_lib/config.py
+++ src/my_lib/config.py
@@ -9,7 +9,9 @@ CONFIG_PATH = "config.yaml"
 
 def load(config_path=CONFIG_PATH):
     config_path = pathlib.Path(config_path).resolve()
+
     logging.info("Load config: %s", config_path)
+
     with config_path.open(mode="r") as file:
         return yaml.load(file, Loader=yaml.SafeLoader)
 
