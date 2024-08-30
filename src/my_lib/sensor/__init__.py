c1c4aa5504e851a5f1d81e6e6a0abb9f27a3216e
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-08-31 00:03:11 +0900

    fix

--- src/my_lib/sensor/__init__.py
+++ src/my_lib/sensor/__init__.py
@@ -1,4 +1,5 @@
 # noqa: D104
+import importlib
 import logging
 import pathlib
 
@@ -8,10 +9,15 @@ from .ads1015 import ADS1015 as ads1015  # noqa: N811
 from .apds9250 import APDS9250 as apds9250  # noqa: N811
 from .ezo_ph import EZO_PH as ezo_ph  # noqa: N811
 from .ezo_rtd import EZO_RTD as ezo_rtd  # noqa: N811
+from .fd_q10c import FD_Q10C as fd_q10c  # noqa: N811
 from .grove_tds import GROVE_TDS as grove_tds  # noqa: N811
 from .i2cbus import I2CBUS as i2cbus  # noqa: N811
 from .scd4x import SCD4X as scd4x  # noqa: N811
 from .sht35 import SHT35 as sht35  # noqa: N811
+
+iolink = importlib.import_module(".io_link", __package__)
+
+__all__ = []
 
 __all__ = [
     "ads1015",
@@ -22,6 +28,8 @@ __all__ = [
     "i2cbus",
     "scd4x",
     "sht35",
+    "fd_q10c",
+    "iolink",
 ]
 
 
