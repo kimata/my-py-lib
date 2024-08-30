b0102022f60e495595cc2f097b7ba24f5c62f9d7
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-08-30 15:13:40 +0900

    feat: AtlasScientific の EZO-RTD に対応

---
+++ src/my_lib/sensor/ezo_rtd.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+"""
+EZO-RTD を使って水温を取得するライブラリです．
+
+Usage:
+  ezo_rtd.py [-b BUS] [-d DEV_ADDR]
+
+Options:
+  -b BUS        : I2C バス番号．[default: 0x01]
+  -d DEV_ADDR   : デバイスアドレス(7bit)． [default: 0x66]
+"""
+
+import logging
+import struct
+import time
+
+import my_lib.sensor.i2cbus
+from my_lib.sensor.i2cbus import I2CBUS
+
+
+class EZO_RTD:
+    NAME = "EZO-RTD"
+    TYPE = "I2C"
+    DEV_ADDR = 0x66  # 7bit
+
+    def __init__(self, bus_id=I2CBUS.ARM, dev_addr=DEV_ADDR):  # noqa: D107
+        self.bus_id = bus_id
+        self.dev_addr = dev_addr
+        self.i2cbus = my_lib.sensor.i2cbus(bus_id)
+
+    def ping(self):
+        logging.debug("ping to dev:0x%02X, bus:0x%02X", self.dev_addr, self.bus_id)
+
+        try:
+            value = self.exec_command("i")
+
+            return value[1:].decode().split(",")[1] == "RTD"
+        except Exception:
+            logging.debug("Failed to detect %s", self.NAME, exc_info=True)
+            return False
+
+    def get_value(self):
+        value = self.exec_command("R")
+
+        return float(value[1:].decode().rstrip("\x00"))
+
+    def exec_command(self, cmd):
+        command = list(cmd.encode())
+
+        self.i2cbus.i2c_rdwr(self.i2cbus.msg.write(self.dev_addr, command))
+
+        time.sleep(1)
+
+        read = self.i2cbus.msg.read(self.dev_addr, 10)
+        self.i2cbus.i2c_rdwr(read)
+
+        return bytes(read)
+
+    def get_value_map(self):
+        value = self.get_value()
+
+        return {"temp": value}
+
+
+if __name__ == "__main__":
+    # TEST Code
+    import docopt
+
+    import my_lib.logger
+    import my_lib.sensor.ezo_rtd
+
+    args = docopt.docopt(__doc__)
+    bus_id = int(args["-b"], 0)
+    dev_addr = int(args["-d"], 0)
+
+    my_lib.logger.init("sensors.scd4x", level=logging.DEBUG)
+
+    sensor = my_lib.sensor.ezo_rtd(bus_id=bus_id, dev_addr=dev_addr)
+
+    ping = sensor.ping()
+    logging.info("PING: %s", ping)
+    if ping:
+        logging.info("VALUE: %s", sensor.get_value_map())
