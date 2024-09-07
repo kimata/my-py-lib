498a5d3a7c52b61410e0963d299040c4c5114d38
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-09-07 22:15:26 +0900

    fix: JSON Schema の雛形生成機能を追加

--- src/my_lib/config.py
+++ src/my_lib/config.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
+"""
+YAML 型式で定義された設定ファイルを読み込みます．
+
+Usage:
+  rasp-aqua.py [-c CONFIG] [-S] [-d]
+
+Options:
+  -c CONFIG         : CONFIG を設定ファイルとして読み込んで実行します．[default: config.yaml]
+  -S                : YAML 記述をもとにして，JSON Schema の雛形を生成します．
+  -d                : デバッグモードで動作します．
+"""
+
+import json
 import logging
 import pathlib
 
+import genson
 import yaml
 
 CONFIG_PATH = "config.yaml"
@@ -16,9 +30,30 @@ def load(config_path=CONFIG_PATH):
         return yaml.load(file, Loader=yaml.SafeLoader)
 
 
+# NOTE: スキーマの雛形を生成
+def generate_schema(config_path):
+    with pathlib.Path(config_path).open() as file:
+        builder = genson.SchemaBuilder()
+        builder.add_object(yaml.load(file, Loader=yaml.SafeLoader))
+
+        print(json.dumps(builder.to_schema(), indent=4))
+
+
 if __name__ == "__main__":
-    import logger
+    import pprint
 
-    logger.init("test", level=logging.INFO)
+    import docopt
+    import my_lib.logger
 
-    load()
+    args = docopt.docopt(__doc__)
+
+    config_file = args["-c"]
+    schema_mode = args["-S"]
+    debug_mode = args["-d"]
+
+    my_lib.logger.init("my-lib.config", level=logging.DEBUG if debug_mode else logging.INFO)
+
+    if schema_mode:
+        generate_schema(config_file)
+    else:
+        pprint.pprint(load(config_file))
