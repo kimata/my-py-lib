bf5c2d7cc3f77da07fe5c883b778abaa6ee9c48a
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-11-19 23:38:43 +0900

    feat: WAV の再生に対応

--- src/my_lib/voice.py
+++ src/my_lib/voice.py
@@ -13,8 +13,13 @@ Options:
   -o WAV            : 書き出す音声ファイル．[default: text.wav]
 """
 
+import audioop
+import io
 import logging
 import urllib.request
+import wave
+
+import simpleaudio
 
 
 def get_query_url(config, text, speaker_id):
@@ -33,6 +38,33 @@ def get_synthesis_url(config, speaker_id):
     )
 
 
+# MEMO: 一般的なサンプリングレートに変更
+def convert_wav_data(wav_data_in):
+    with wave.open(io.BytesIO(wav_data_in), "rb") as wav_in:
+        n_channels = wav_in.getnchannels()
+        sampwidth = wav_in.getsampwidth()
+        framerate = wav_in.getframerate()
+        n_frames = wav_in.getnframes()
+        comptype = wav_in.getcomptype()
+        compname = wav_in.getcompname()
+
+        audio_data = wav_in.readframes(n_frames)
+
+        if framerate != 44100:
+            audio_data = audioop.ratecv(audio_data, sampwidth, n_channels, framerate, 44100, None)[0]
+            framerate = 44100
+
+        with io.BytesIO() as wav_data_out:
+            with wave.open(wav_data_out, "wb") as wav_out:
+                wav_out.setnchannels(n_channels)
+                wav_out.setsampwidth(sampwidth)
+                wav_out.setframerate(framerate)
+                wav_out.setcomptype(comptype, compname)
+                wav_out.writeframes(audio_data)
+
+            return wav_data_out.getvalue()
+
+
 def synthesize(config, text, speaker_id=3):
     req = urllib.request.Request(get_query_url(config, text, speaker_id), method="POST")  # noqa: S310
     res = urllib.request.urlopen(req)  # noqa: S310
@@ -42,7 +74,12 @@ def synthesize(config, text, speaker_id=3):
     req.add_header("Content-Type", "application/json")
     res = urllib.request.urlopen(req)  # noqa: S310
 
-    return res.read()
+    return convert_wav_data(res.read())
+
+
+def play(wav_data):
+    wave_obj = simpleaudio.WaveObject.from_wave_file(io.BytesIO(wav_data))
+    wave_obj.play().wait_done()
 
 
 if __name__ == "__main__":
@@ -65,5 +102,8 @@ if __name__ == "__main__":
     config = my_lib.config.load(config_file)
 
     wav_data = synthesize(config, message, speaker_id)
+
     with pathlib.Path(out_file).open("wb") as f:
         f.write(wav_data)
+
+    play(wav_data)
