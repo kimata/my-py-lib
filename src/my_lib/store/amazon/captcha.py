5add5ad493ad057633b6816a91c6d7b2244652e8
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-12-07 00:24:27 +0900

    fix: まだリファクタリング中だけど，一旦 commit

--- src/my_lib/store/amazon/captcha.py
+++ src/my_lib/store/amazon/captcha.py
@@ -9,8 +9,80 @@ import PIL.Image
 import selenium.webdriver.common.by
 import selenium.webdriver.support
 
+# def resolve_impl(driver, wait, config, xpath):
+#     file_id = my_lib.store.captcha.send_challenge_image_slack(
+#         config["slack"]["bot_token"],
+#         config["slack"]["captcha"]["channel"]["id"],
+#         "Amazon Login",
+#         PIL.Image.open(
+#             io.BytesIO(
+#                 driver.find_element(selenium.webdriver.common.by.By.XPATH, xpath["image"]).screenshot_as_png
+#             )
+#         ),
+#         "画像 CAPTCHA",
+#     )
 
-def resolve_impl(driver, wait, config, xpath):
+#     captcha = my_lib.store.captcha.recv_response_image_slack(
+#         config["slack"]["bot_token"], config["slack"]["captcha"]["channel"]["id"], file_id
+#     )
+
+#     if captcha is None:
+#         raise "CAPTCHA を解決できませんでした．"
+
+#     driver.find_element(selenium.webdriver.common.by.By.XPATH, xpath["text"]).send_keys(captcha)
+
+
+# def resolve(driver, wait, config):
+#     if len(driver.find_elements(selenium.webdriver.common.by.By.XPATH, '//button[@type="submit"]')) == 0:
+#         return
+
+#     logging.info("Try to resolve CAPTCHA")
+
+#     while True:
+#         resolve_impl(
+#             driver,
+#             wait,
+#             config,
+#             {
+#                 "image": '//form[@action="/errors/validateCaptcha"]//img',
+#                 "text": '//input[@id="captchacharacters"]',
+#             },
+#         )
+
+#         driver.find_element(selenium.webdriver.common.by.By.XPATH, '//button[@type="submit"]').click()
+
+#         wait.until(selenium.webdriver.support.expected_conditions.presence_of_all_elements_located)
+#         time.sleep(0.1)
+
+#         if len(driver.find_elements(selenium.webdriver.common.by.By.XPATH, '//button[@type="submit"]')) == 0:
+#             break
+
+#     logging.info("Broke through CAPTCHA")
+
+
+# def resolve_captcha(handle, queue_name, xpath):
+#     driver, wait = get_queue_dirver(handle, queue_name)
+#     config = handle["config"]
+
+#     file_id = captcha_slack.send_challenge_image(
+#         config["slack"]["bot_token"],
+#         config["slack"]["captcha"]["channel"]["id"],
+#         "Amazon Login",
+#         PIL.Image.open(io.BytesIO(driver.find_element(By.XPATH, xpath["image"]).screenshot_as_png)),
+#         "画像 CAPTCHA",
+#     )
+
+#     captcha = captcha_slack.recv_response(
+#         config["slack"]["bot_token"], config["slack"]["captcha"]["channel"]["id"], "image", file_id
+#     )
+
+#     if captcha is None:
+#         raise "CAPTCHA を解決できませんでした．"
+
+#     driver.find_element(By.XPATH, xpath["text"]).send_keys(captcha)
+
+
+def resolve(driver, wait, config, xpath):
     file_id = my_lib.store.captcha.send_challenge_image_slack(
         config["slack"]["bot_token"],
         config["slack"]["captcha"]["channel"]["id"],
@@ -24,7 +96,7 @@ def resolve_impl(driver, wait, config, xpath):
     )
 
     captcha = my_lib.store.captcha.recv_response_image_slack(
-        config["slack"]["bot_token"], config["slack"]["captcha"]["channel"]["id"], file_id
+        config["slack"]["bot_token"], config["slack"]["captcha"]["channel"]["id"], "image", file_id
     )
 
     if captcha is None:
@@ -33,29 +105,52 @@ def resolve_impl(driver, wait, config, xpath):
     driver.find_element(selenium.webdriver.common.by.By.XPATH, xpath["text"]).send_keys(captcha)
 
 
-def resolve(driver, wait, config):
-    if len(driver.find_elements(selenium.webdriver.common.by.By.XPATH, '//button[@type="submit"]')) == 0:
-        return
+# def scrape_resolve_captcha(handle, queue_name):
+#     driver, wait = get_queue_dirver(handle, queue_name)
 
-    logging.info("Try to resolve CAPTCHA")
+#     if len(driver.find_elements(By.XPATH, '//button[@type="submit"]')) == 0:
+#         return
 
-    while True:
-        resolve_impl(
-            driver,
-            wait,
-            config,
-            {
-                "image": '//form[@action="/errors/validateCaptcha"]//img',
-                "text": '//input[@id="captchacharacters"]',
-            },
-        )
+#     logging.info("Try to resolve CAPTCHA")
 
-        driver.find_element(selenium.webdriver.common.by.By.XPATH, '//button[@type="submit"]').click()
+#     while True:
+#         resolve_captcha(
+#             handle,
+#             queue_name,
+#             {
+#                 "image": '//form[@action="/errors/validateCaptcha"]//img',
+#                 "text": '//input[@id="captchacharacters"]',
+#             },
+#         )
 
-        wait.until(selenium.webdriver.support.expected_conditions.presence_of_all_elements_located)
-        time.sleep(0.1)
+#         driver.find_element(By.XPATH, '//button[@type="submit"]').click()
 
-        if len(driver.find_elements(selenium.webdriver.common.by.By.XPATH, '//button[@type="submit"]')) == 0:
-            break
+#         wait.until(EC.presence_of_all_elements_located)
+#         time.sleep(0.1)
 
-    logging.info("Broke through CAPTCHA")
+#         if len(driver.find_elements(By.XPATH, '//button[@type="submit"]')) == 0:
+#             break
+
+#     logging.info("Broke through CAPTCHA")
+
+
+# def resolve_captcha(handle, queue_name, xpath):
+#     driver, wait = get_queue_dirver(handle, queue_name)
+#     config = handle["config"]
+
+#     file_id = captcha_slack.send_challenge_image(
+#         config["slack"]["bot_token"],
+#         config["slack"]["captcha"]["channel"]["id"],
+#         "Amazon Login",
+#         PIL.Image.open(io.BytesIO(driver.find_element(By.XPATH, xpath["image"]).screenshot_as_png)),
+#         "画像 CAPTCHA",
+#     )
+
+#     captcha = captcha_slack.recv_response(
+#         config["slack"]["bot_token"], config["slack"]["captcha"]["channel"]["id"], "image", file_id
+#     )
+
+#     if captcha is None:
+#         raise "CAPTCHA を解決できませんでした．"
+
+#     driver.find_element(By.XPATH, xpath["text"]).send_keys(captcha)
