f2de00257a32eb33d38497f0d2826ae6b5cfe49f
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-12-27 00:06:36 +0900

    fix: テストの安定性を改善

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -107,6 +107,7 @@ def test_webapp_log(client):
     response = client.get(data.sample_webapp.WEBAPP_URL_PREFIX + "/api/log_view")
     assert response.status_code == 200
     log_list = response.json["data"]
+    time.sleep(1)
 
     assert log_list[0]["message"] == "TEST WARN"
     assert log_list[1]["message"] == "TEST ERROR"
