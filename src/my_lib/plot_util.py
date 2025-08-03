c14d2fcb37cc1370e45474afd8e6bbe4e67b35b8
KIMATA Tetsuya <kimata@green-rabbit.net>
2025-08-03 14:36:32 +0900

    fix

--- src/my_lib/plot_util.py
+++ src/my_lib/plot_util.py
@@ -12,8 +12,8 @@ def _get_font_properties(font_path_str, size):
     return matplotlib.font_manager.FontProperties(fname=font_path_str, size=size)
 
 
-def get_plot_font(config, font_type, size):
-    font_path = pathlib.Path(config["path"]).resolve() / config["map"][font_type]
+def get_plot_font(font_config, font_type, size):
+    font_path = pathlib.Path(font_config["path"]).resolve() / font_config["map"][font_type]
 
     cache_info = _get_font_properties.cache_info()
 
@@ -22,6 +22,6 @@ def get_plot_font(config, font_type, size):
 
     # キャッシュミスが増えた場合は新しいフォントのロード
     if new_cache_info.misses > cache_info.misses:
-        logging.info("Load font: %s (cached)", font_path)
+        logging.debug("Load font: %s (cached)", font_path)
 
     return result
